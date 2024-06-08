29c5658c14e571da2f1d4466cf313e9415d3a7cb
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-06-08 11:45:13 -0500

    cotainer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:recetas_app_g8/widgets/form_item.dart';
 
 class HomePage extends StatelessWidget {
-  TextEditingController correoController = new TextEditingController();
+  TextEditingController titleController = new TextEditingController();
+  TextEditingController decripcionController = new TextEditingController();
+  TextEditingController urlImageController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -13,48 +16,88 @@ class HomePage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            TextField(
+            FormItem(
+              controlador: titleController,
+              hint: "Ingresa el nombre del platillo",
+              icono: Icons.title,
+            ),
+            FormItem(
+              controlador: decripcionController,
+              hint: "Ingresa la descripción",
+              icono: Icons.description,
+            ),
+            FormItem(
+              controlador: urlImageController,
+              hint: "Ingresa la url de la imagen",
+              icono: Icons.image,
+            ),
+
+            Text(
+              "Listado General",
               style: TextStyle(
-                color: Colors.red,
                 fontSize: 25,
+                color: Colors.white,
                 fontWeight: FontWeight.bold,
               ),
-              cursorColor: Colors.orange,
-              controller: correoController,
-              decoration: InputDecoration(
-                prefix: Text("prefix/ "),
-                prefixIcon: Icon(Icons.abc),
-                // suffix: Text("/suffix"),
-                suffixIcon: Icon(Icons.safety_check),
-                suffixText: "suffixtext",
-                // prefixText: "prefixtext",
-                contentPadding: EdgeInsets.all(15),
-                label: Text("LABEL"),
-                labelStyle: TextStyle(color: Colors.pink, fontSize: 12),
-                hintText: "Ingresa tu correo",
-                hintStyle: TextStyle(color: Colors.cyan),
-                filled: true,
-                fillColor: Colors.grey.shade300,
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide.none,
-                  borderRadius: BorderRadius.circular(25),
-                ),
-                focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.red, width: 4),
-                    borderRadius: BorderRadius.circular(25)),
-              ),
-              onChanged: (String mandarina) {
-                print(mandarina);
-                print(correoController.text);
-              },
             ),
-            ElevatedButton(
-              onPressed: () {
-                print(correoController.text);
-              },
-              child: Text("imprimir entrada de usuario"),
-            )
+
+            Container(
+              padding: EdgeInsets.all(16),
+              width: double.infinity,
+              height: 300,
+              decoration: BoxDecoration(
+                color: Color(0xff29304B).withOpacity(0.9),
+                borderRadius: BorderRadius.circular(16),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black,
+                    offset: Offset(5, 5),
+                    blurRadius: 10,
+                  ),
+                ],
+              ),
+              child: Column(
+                children: [
+                  // Container(
+                  //     height: 200,
+                  //     decoration: BoxDecoration(
+                  //       borderRadius: BorderRadius.circular(16),
+                  //       image: DecorationImage(
+                  //         fit: BoxFit.cover,
+                  //         image: NetworkImage(
+                  //             "https://www.solopostres.com/wp-content/uploads/2016/04/receta198.jpg"),
+                  //       ),
+                  //     ))
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(16),
+                    child: Image.network(
+                      "https://www.solopostres.com/wp-content/uploads/2016/04/receta198.jpg",
+                      height: 200,
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Text(
+                    "Wafles",
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                  Text(
+                    "Los waffles son originarios de Bélgica, y también son llamados “Gofre” (palabra proveniente del francés). Su preparación no es complicada en absoluto y son muy apropiados para ser usados como base para otros postres. Las combinaciones con helados, cremas o frutas son ideales para saborear esta delicia culinaria.",
+                    style: TextStyle(color: Colors.white),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  )
+                ],
+              ),
+            ),
+            // ElevatedButton(
+            //   onPressed: () {
+            //     // print(correoController.text);
+            //   },
+            //   child: Text("imprimir entrada de usuario"),
+            // )
           ],
         ),
       ),
---
+++ lib/widgets/form_item.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class FormItem extends StatelessWidget {
+  TextEditingController controlador;
+  String hint;
+  IconData icono;
+
+  FormItem(
+      {required this.controlador, required this.hint, required this.icono});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8),
+      child: TextField(
+        style: TextStyle(color: Colors.white),
+        cursorColor: Colors.orange.shade700,
+        controller: controlador,
+        decoration: InputDecoration(
+          prefixIcon: Icon(icono),
+          contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+          hintText: hint,
+          hintStyle: TextStyle(color: Colors.white),
+          filled: true,
+          fillColor: Color(0xff626B92),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(25),
+          ),
+          focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide.none,
+              borderRadius: BorderRadius.circular(25)),
+        ),
+      ),
+    );
+  }
+}
