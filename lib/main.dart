ea1d54b5a12266650023f8edcca81d1230a33b73
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-06-08 09:23:23 -0500

    init

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:recetas_app_g8/pages/home_page.dart';
+
+void main() {
+  MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  );
+}
