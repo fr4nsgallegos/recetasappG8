5b9afdd1ea1528d081bec0a78f419a65a7f20edb
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-06-09 12:02:32 -0500

    iam herader

--- lib/pages/reto_container_page.dart
+++ lib/pages/reto_container_page.dart
@@ -8,28 +8,71 @@ class RetoContainerPage extends StatelessWidget {
         title: Text("RETOS CONTAINER"),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              shape: BoxShape.circle,
-              border: Border.all(
-                color: Colors.orange,
-                width: 4,
-              ),
-            ),
-            alignment: Alignment.center,
-            child: Text(
-              "H",
-              style: TextStyle(
-                fontSize: 200,
-                color: Colors.orange,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          )
+          IamHeaderWidget(),
+          Helipuerto(),
         ],
+      ),
+    );
+  }
+}
+
+class IamHeaderWidget extends StatelessWidget {
+  const IamHeaderWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(25),
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.only(
+          bottomRight: Radius.circular(25),
+          bottomLeft: Radius.circular(25),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.blue,
+            offset: Offset(5, 5),
+            blurRadius: 10,
+          ),
+        ],
+      ),
+      alignment: Alignment.center,
+      child: Text(
+        "I am a header",
+        style: TextStyle(
+          fontSize: 35,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
+
+class Helipuerto extends StatelessWidget {
+  const Helipuerto({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: Colors.orange,
+          width: 4,
+        ),
+      ),
+      alignment: Alignment.center,
+      child: Text(
+        "H",
+        style: TextStyle(
+          fontSize: 200,
+          color: Colors.orange,
+          fontWeight: FontWeight.bold,
+        ),
       ),
     );
   }
