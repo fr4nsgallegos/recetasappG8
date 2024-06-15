97e9c155b70796aea900b7376c2abf31dd52fd1b
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-06-15 13:02:46 -0500

    error

--- lib/pages/whatsapp/status_page.dart
+++ lib/pages/whatsapp/status_page.dart
@@ -5,7 +5,35 @@ class StatusPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        ListTile(),
+        ListTile(
+          leading: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              CircleAvatar(
+                radius: 22,
+                backgroundImage: NetworkImage(
+                    "https://www.wilsoncenter.org/sites/default/files/media/images/person/james-person-1.jpg"),
+              ),
+              Positioned(
+                child: Container(
+                  decoration: BoxDecoration(shape: BoxShape.circle),
+                ),
+              )
+            ],
+          ),
+          title: Text(
+            "My status",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          // subtitle: Text(
+          //   "Hola como estas?",
+          //   overflow: TextOverflow.ellipsis,
+          //   maxLines: 1,
+          // ),
+        ),
       ],
     );
   }
