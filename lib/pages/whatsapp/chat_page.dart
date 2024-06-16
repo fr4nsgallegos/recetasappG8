c9148b48d1f9774664c1d954fd2a26bfd9f719d9
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-06-16 10:11:39 -0500

    cached network image

--- lib/pages/whatsapp/chat_page.dart
+++ lib/pages/whatsapp/chat_page.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:recetas_app_g8/pages/whatsapp/widgets_whatsapp/chat_item.dart';
 
@@ -17,6 +18,28 @@ class _ChatPageState extends State<ChatPage> {
         ChatItem(),
         ChatItem(),
         ChatItem(),
+        Container(
+          child: CachedNetworkImage(
+            progressIndicatorBuilder: (context, url, progress) => Center(
+              child: CircularProgressIndicator(
+                value: progress.progress,
+              ),
+            ),
+            imageUrl:
+                "https://www.wilsoncenter.org/sites/default/files/media/images/person/james-person-1.jpg",
+            imageBuilder: (context, imageProvider) => Container(
+              width: 200,
+              height: 200,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),
+                image: DecorationImage(
+                  image: imageProvider,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+        ),
       ],
     );
   }
--- lib/pages/whatsapp/widgets_whatsapp/chat_item.dart
+++ lib/pages/whatsapp/widgets_whatsapp/chat_item.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class ChatItem extends StatelessWidget {
@@ -6,11 +7,34 @@ class ChatItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: CircleAvatar(
-        radius: 28,
-        backgroundImage: NetworkImage(
-            "https://www.wilsoncenter.org/sites/default/files/media/images/person/james-person-1.jpg"),
+      leading: SizedBox(
+        height: 56,
+        width: 56,
+        child: CachedNetworkImage(
+          imageUrl:
+              "https://www.wilsoncenter.org/sites/default/files/media/images/person/james-person-1.jpg",
+          progressIndicatorBuilder: (context, url, progress) => Center(
+            child: Container(
+              height: 20,
+              width: 20,
+              child: CircularProgressIndicator(
+                color: Colors.green,
+                // strokeWidth: 10, //ancho de la barra de carga
+                value: progress.progress,
+              ),
+            ),
+          ),
+          imageBuilder: (context, imageProvider) => CircleAvatar(
+            backgroundImage: imageProvider,
+            radius: 28,
+          ),
+        ),
       ),
+      //  CircleAvatar(
+      //   radius: 28,
+      //   backgroundImage: NetworkImage(
+      //       "https://www.wilsoncenter.org/sites/default/files/media/images/person/james-person-1.jpg"),
+      // ),
       title: Text(
         "Nombre de la person",
         style: TextStyle(
